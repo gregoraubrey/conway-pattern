a6aa64dca33a1262363388f7fd26ad6e1b5e0a86
gregor <gregor.aubrey.uk@gmail.com>
2024-04-01 11:02:56 +0100

    create calculateNextNumber

--- main.go
+++ main.go
@@ -4,9 +4,9 @@ import (
 	"bufio"
 	"fmt"
 	"io"
-	"strings"
-	"strconv"
 	"os"
+	"strconv"
+	"strings"
 )
 
 func main() {
@@ -14,6 +14,8 @@ func main() {
 	reader := bufio.NewReader(os.Stdin)
 	num1, num2 := getTwoNumbersFromUser(reader)
 	fmt.Println(num1, num2)
+	nextNum := calculateNextNumber(num1, num2)
+	fmt.Printf("The next number in the sequence is: %d\n", nextNum)
 	fmt.Println("The end.")
 }
 
@@ -40,4 +42,34 @@ func getTwoNumbersFromUser(reader io.Reader) (int, int) {
 			}
 		}
 	}
+}
+
+func calculateNextNumber(num1 int, num2 int) int {
+	// Calculate whether num1 and num2's sum is prime
+	if isPrime(num1 + num2) {
+		return num1 + num2
+	}
+	// Otherwise, extract the lowest prime factor of num1 + num2
+	lpf := getLowestPrimeFactor(num1 + num2)
+	// Then return the sum of num1 and num2 divided by its lowest prime factor
+	return (num1 + num2) / lpf
+}
+
+func isPrime(num int) bool {
+	for i := 2; i < num; i++ {
+		if num%i == 0 {
+			return false
+		}
+	}
+	return true
+}
+
+func getLowestPrimeFactor(num int) int {
+	for i := 2; i < num; i++ {
+		if num%i == 0 && isPrime(i) {
+			return i
+		}
+	}
+	fmt.Println("Error: No prime factor found. Returning 1 instead.")
+	return 1
 }
