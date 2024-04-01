ca147896d6fd27ee870b680acf3dc073959d1cb8
gregor <gregor.aubrey.uk@gmail.com>
2024-04-01 11:46:17 +0100

    add test for buildSequence

--- main_test.go
+++ main_test.go
@@ -1,6 +1,9 @@
 package main
 
-import "testing"
+import (
+	"testing"
+	"reflect"
+)
 
 func TestCalculateNextNumber(t *testing.T) {
 	t.Run("Test CalculateNextNumber with 1 and 1", func(t *testing.T) {
@@ -36,6 +39,51 @@ func TestCalculateNextNumber(t *testing.T) {
 		expected := 25
 		if result != expected {
 			t.Errorf("result %d expected %d", result, expected)
+		}
+	})
+}
+
+func TestBuildSequence(t *testing.T) {
+	t.Run("Test buildSequence with 1, 1, 5", func(t *testing.T) {
+		result := buildSequence(1, 1, 5)
+		expected := []int{1, 1, 2, 3, 5}
+		if !reflect.DeepEqual(result, expected) {
+			t.Errorf("result %v expected %v", result, expected)
+		}
+	})
+	t.Run("Test buildSequence with 2, 3, 5", func(t *testing.T) {
+		result := buildSequence(2, 3, 5)
+		expected := []int{2, 3, 5, 4, 3}
+		if !reflect.DeepEqual(result, expected) {
+			t.Errorf("result %v expected %v", result, expected)
+		}
+	})
+	t.Run("Test buildSequence with 3, 5, 5", func(t *testing.T) {
+		result := buildSequence(3, 5, 5)
+		expected := []int{3, 5, 4, 3, 7}
+		if !reflect.DeepEqual(result, expected) {
+			t.Errorf("result %v expected %v", result, expected)
+		}
+	})
+	t.Run("Test buildSequence with 5, 8, 5", func(t *testing.T) {
+		result := buildSequence(5, 8, 5)
+		expected := []int{5, 8, 13, 7, 10}
+		if !reflect.DeepEqual(result, expected) {
+			t.Errorf("result %v expected %v", result, expected)
+		}
+	})
+	t.Run("Test buildSequence with 30, 20, 5", func(t *testing.T) {
+		result := buildSequence(30, 20, 5)
+		expected := []int{30, 20, 25, 15, 20}
+		if !reflect.DeepEqual(result, expected) {
+			t.Errorf("result %v expected %v", result, expected)
+		}
+	})
+	t.Run("Test buildSequence with 1, 1, 2", func(t *testing.T) {
+		result := buildSequence(1, 1, 2)
+		expected := []int{1, 1}
+		if !reflect.DeepEqual(result, expected) {
+			t.Errorf("result %v expected %v", result, expected)
 		}
 	})
 }
